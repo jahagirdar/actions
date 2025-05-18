81b081af5de49f5e69aa9abcaaf24851ddb7e684
Vijayvithal Jahagirdar <jvs@dyumnin.com>
2025-05-18 14:58:22 +0530

    Run tests.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -l
 
+make -C /github/workspace/tests COCOTB_RESULTS_FILE=dut_results.xml
 echo "Hello $1"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
-cat /github/workspace/README.md
+ls /github/workspace/tests
 
 
