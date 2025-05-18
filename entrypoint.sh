2f8ab794c4f74df70c9377ccfb505a291ec80c59
Vijayvithal Jahagirdar <jvs@dyumnin.com>
2025-05-18 15:17:42 +0530

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -l
 echo `ls /`
 echo `ls /github`
+echo `ls -al /github/workspace`
+cd /github/workspace && git checkout 
 echo `ls /github/workspace`
 make -C /github/workspace/tests COCOTB_RESULTS_FILE=dut_results.xml
 echo "Hello $1"
